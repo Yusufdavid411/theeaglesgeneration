dca77b39bb0f85945e5e55e878903efbda27ccac
David <yusufdavid411@gmail.com>
2025-03-03 10:41:00 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="content" id="content">
+      <Router>
+
+
+        <Header />
+
+
+      </Router>
+    </div>
+  )
+}
+
+export default App
