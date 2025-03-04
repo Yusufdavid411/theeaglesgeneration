1e3db0748afde022c1765afb26b73dcd45add025
David <yusufdavid411@gmail.com>
2025-03-04 04:42:39 +0100

    menu added

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
+import "./header.css";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav>
-      <ul>
-        <li>HOME</li>
-        <li>Discover Us</li>
-        <li>Download</li>
-        <li>Our Webtree</li>
-        <li>Our Channeltree</li>
-        <li>Give To Us Today</li>
-      </ul>
-  </nav>
-  )
-}
+    <nav className="navbar">
+      {/* Hamburger Menu Button */}
+      <div className="menu-icon" onClick={() => setMenuOpen(true)}>
+        ☰
+      </div>
 
-export default Navbar
+      {/* Full-Screen Menu */}
+      <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+        {/* Close Button */}
+        <div className="close-btn" onClick={() => setMenuOpen(false)}>✖</div>
+
+        {/* Menu Items */}
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -6,6 +6,17 @@ const Section = () => {
       <div>Section</div>
       <div>Section</div>
       <div>Section</div>
+
+      <nav>
+        <ul>
+          <li>HOME</li>
+          <li>Discover Us</li>
+          <li>Download</li>
+          <li>Our Webtree</li>
+          <li>Our Channeltree</li>
+          <li>Give To Us Today</li>
+        </ul>
+      </nav>
     </section>
    );
 }
